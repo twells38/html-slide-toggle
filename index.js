d4299b87cc176eba5436ca9283a7fe18257409d4
Sornya 2 <sornya2@Users-MacBook-Air.local>
2021-09-23 14:32:11 -0400

    deploy to github

---
+++ index.js
@@ -0,0 +1,25 @@
+$("button").on("click", function() {
+  $(".text").slideToggle(500);
+});
+
+//$("button").on("click", function() {
+//  $(".text").slideDown();
+//});
+
+//$("button").on("click", function() {
+//  $("div").slideUp();
+//});
+
+//$("button").on("click", function() {
+// $("div").fadeToggle(500);
+//});
+
+//$("button").on("click", function() {
+//  $("div").fadeIn(1000, function() {});
+//});
+
+//$("button").on("click",function(){
+//$('div').fadeOut(1000, function(){
+
+//});
+//});
